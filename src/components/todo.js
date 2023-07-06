ca791b2e6dedb89c9ed673f8e12be50126a5a430
Sowska <losowskisofia@gmail.com>
2023-07-06 13:08:36 -0300

    Deploy config

--- src/components/todo.js
+++ src/components/todo.js
@@ -2,43 +2,43 @@ import { useState } from "react";
 export default function Todo({ item, onUpdate, onDelete }) {
     const [isEdit, setIsEdit] = useState(false);
 
-    function FormEdit(){
+    function FormEdit() {
 
-        const[newValue, setNewValue] = useState(item.title);
-        function handleSubmit(e){
-            e.preventDefault();        
+        const [newValue, setNewValue] = useState(item.title);
+        function handleSubmit(e) {
+            e.preventDefault();
         }
 
-    function handleChange(e){
-        const value =e.target.value;
-        setNewValue(value);
-    }
+        function handleChange(e) {
+            const value = e.target.value;
+            setNewValue(value);
+        }
 
-    function handleClickUpdateTodo(){
-        onUpdate(item.id, newValue);
-        setIsEdit(false);
+        function handleClickUpdateTodo() {
+            onUpdate(item.id, newValue);
+            setIsEdit(false);
 
-    }
+        }
 
         return (
-        <form className="todoUpdateForm" onClick={handleSubmit}>
-        <input type="text" className="todoInput" onChange={handleChange} value={newValue}/>
-        <button className="button" onClick={handleClickUpdateTodo} >Update</button>
-        </form>
+            <form className="todoUpdateForm" onClick={handleSubmit}>
+                <input type="text" className="todoInput" onChange={handleChange} value={newValue} />
+                <button className="button" onClick={handleClickUpdateTodo} >Update</button>
+            </form>
         );
 
     }
-    function TodoElement(){
+    function TodoElement() {
         return <div className="todoInfo">
             <span className="todoTitle">{item.title} </span>
-        <button className="button" onClick={() => setIsEdit(true)}>Edit</button>
-        <button className="buttonDelete" onClick={(e) => onDelete(item.id)}>Delete</button>
-    </div>
+            <button className="button" onClick={() => setIsEdit(true)}>Edit</button>
+            <button className="buttonDelete" onClick={(e) => onDelete(item.id)}>Delete</button>
+        </div>
     }
     return (
 
         <div className="todo">
-            {isEdit ? <FormEdit/> : <TodoElement />}
+            {isEdit ? <FormEdit /> : <TodoElement />}
         </div>
 
     );
